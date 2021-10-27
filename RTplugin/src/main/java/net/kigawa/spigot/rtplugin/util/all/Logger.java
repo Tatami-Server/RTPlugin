2c3194a3ecc11e345e052e68aaa6a27073c54c29
kigawa <72866494+kigawa8390@users.noreply.github.com>
2021-10-27 21:29:33 +0900

    Delete RTplugin directory

--- RTplugin/src/main/java/net/kigawa/spigot/rtplugin/util/all/Logger.java
+++
@@ -1,5 +0,0 @@
-package net.kigawa.spigot.rtplugin.util.all;
-
-public interface Logger {
-    public void logger(String message);
-}
--- RTplugin/src/main/java/net/kigawa/spigot/rtplugin/util/plugin/all/command/Subcommand.java
+++
@@ -1,9 +0,0 @@
-package net.kigawa.spigot.rtplugin.util.plugin.all.command;
-
-import net.kigawa.spigot.rtplugin.util.plugin.all.PluginBase;
-
-public abstract class Subcommand extends Command {
-    public Subcommand(PluginBase kigawaPlugin) {
-        super(kigawaPlugin);
-    }
-}
